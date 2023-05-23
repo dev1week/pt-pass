ef15945835449333b5c1757c2b02d762efc783de
dev1week <rondo2860@gmail.com>
2023-05-24 00:36:57 +0900

    create prj

---
+++ src/main/java/com/week/ptpass/PtPassApplication.java
@@ -0,0 +1,13 @@
+package com.week.ptpass;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PtPassApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PtPassApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/week/ptpass/PtPassApplicationTests.java
@@ -0,0 +1,13 @@
+package com.week.ptpass;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PtPassApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
